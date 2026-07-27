ce179fec301d40ae6b4d013554a590b1c0cf98fd
jaucelabiado5-ops <jaucelabiado5@gmail.com>
2026-07-27 09:09:02 +0000

    DateTask

---
+++ Task/task121/MyDate.java
@@ -0,0 +1,12 @@
+package task121;
+import task121.DateTask;
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask(1,01,1978);
+        DateTask date2 = new DateTask(9,21,1984);
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+       // display leapyears
+        DateTask.leapYears();
+    }
+}
