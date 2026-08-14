e93c9729e6a6b2f63383bb908d684df82c04c650
jaucelabiado5-ops <jaucelabiado5@gmail.com>
2026-08-14 06:00:37 +0000

    odd even negative zero

---
+++ Programchallenge/oddeven.java
@@ -0,0 +1,30 @@
+package Programchallenge;
+
+import java.util.Scanner;
+
+public class oddeven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Check odd or even
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+
+        // Check positive, negative, or zero
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        input.close();
+    }
+}
