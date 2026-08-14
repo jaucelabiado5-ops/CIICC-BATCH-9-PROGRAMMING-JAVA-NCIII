b4b767ba3661b4f7136a8bbd2b993b9409bd33cf
jaucelabiado5-ops <jaucelabiado5@gmail.com>
2026-08-14 06:25:27 +0000

    task2.java

---
+++ Task/Task2.java
@@ -0,0 +1,19 @@
+package Task;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String h = "H";
+        byte three = 3;
+        byte eleven = 11;
+        byte zero = 0;
+        String space = " ";
+        String w = "w";
+        String rld = "rld";
+        byte zero2 = 0;
+        float twoZero = 2.0f;
+        boolean trueVal = true;
+
+        String output = h + three + eleven + zero + space + w + zero2 + rld + space + twoZero + space + trueVal;
+        System.out.println(output);
+    }
+}
