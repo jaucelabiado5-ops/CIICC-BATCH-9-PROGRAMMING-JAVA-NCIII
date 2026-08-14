29f4e3428dcaf5d3734f178ed4b7500cd5f085eb
jaucelabiado5-ops <jaucelabiado5@gmail.com>
2026-08-14 05:57:10 +0000

    number to day

---
+++ Programchallenge/NumbertoDay.java
@@ -0,0 +1,46 @@
+package Programchallenge;
+
+import java.util.Scanner;
+
+public class NumbertoDay {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number (1-7): ");
+        String entry = input.nextLine();
+
+        try {
+            int day = Integer.parseInt(entry);
+
+            switch (day) {
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input");
+        }
+
+        input.close();
+    }
+}
